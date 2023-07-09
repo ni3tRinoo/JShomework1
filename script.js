9ad802604fc181a1911910dd30d8b795271f3744
ni3tRinoo <vladik07@yahoo.com>
2023-07-09 14:20:59 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// //1
+
+// let age = 14;
+
+// console.log(age);
+
+// //2
+
+// let name = "Vladislav";
+
+// console.log(name);
+
+// //3
+
+// let isStudent = "Vladislav";
+
+// console.log(Number.isNaN(isStudent))
+
+// //4
+
+// let myString = "«Одного дня страх постукав y двері. Кураж пішов відкривати, але нікого не знайшов».";
+
+// console.log(myString)
+
+//7
+
+// let number = Number (prompt("Hello Mate"));
+
+//8
+
+// let hello = confirm("ти підтверджуєш?");
+
+// console.log(hello);
+
+// if (hello) {
+//     console.log("Дякую за підтвердження!")
+// } else {
+//     console.log("Дію відмінено!")
+// }
